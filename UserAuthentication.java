8d9277c3c0aa268827c02926187240a5600c0a58
1106695s <schnakovszki@gmail.com>
2013-11-11 13:56:41 +0000

    Fixed structure.

--- UserAuthentication.java
+++
@@ -1,9 +0,0 @@
-public class UserAuthentication implements Authentication {
-
-	private static final String CORRECT_USERNAME = "admin";
-	private static final String CORRECT_PASSWORD = "teamk";
-
-	public boolean checkCredentials(String username, String password) {
-		return (username.compareTo(CORRECT_USERNAME) == 0 && password.compareTo(CORRECT_PASSWORD) == 0);
-	}
-}
