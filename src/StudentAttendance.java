ea6cbd89a9d7776efd9e0561865c61d563014c07
Tomasz Sadowski <1106113s@bo720-5-02.(none)>
2013-11-12 19:15:10 +0000

    1.If we dont use a database here then we have to assume that these records cannot be changed 2.That is the most ugly code I have ever written xD  This whole class has to be changed or we need a database. Or transform these string arrays into some HashMap with keys as course names, session names and student names.

--- src/StudentAttendance.java
+++ src/StudentAttendance.java
@@ -1,11 +1,18 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class StudentAttendance {
 	
 	/* course X sessionType X username */
+	//If we dont use a database here then we have to assume that these records cannot be changed
 	private static final String[] MARTYNAS_PSD3_LAB_ATTENDANCE = {};
 	private static final String[] HELEN_PSD3_LAB_ATTENDANCE = {"2013-09-01"};
 	private static final String[] RALUCA_PSD3_LAB_ATTENDANCE = {"2013-09-01", "2013-09-08"};
 	private static final String[] VLAD_PSD3_LAB_ATTENDANCE = {"2013-09-01", "2013-09-08", "2013-09-15"};
-	private static final String[] TOMASZ_PSD3_LAB_ATTENDANCE = {"2013-09-01", "2013-09-08", "2013-09-15", "2013-09-22"};	
+	private static final String[] TOMASZ_PSD3_LAB_ATTENDANCE = {"2013-09-01", "2013-09-08", "2013-09-15", "2013-09-22"};
+
+		
 	
 	/*
 	 * @param	course      the name of the course we want to retrieve information about, i.e. PSD3, AP3, etc.
@@ -13,21 +20,21 @@ public class StudentAttendance {
 	 * @param	username    the username of the student we are looking to retrieve information about
 	 * @return                   an array of dates that correspond to lab sessions attended
 	 */
-	public static String[] getAttendanceByCourse(String course, String sessionType, String username) {
+	public String[] getAttendanceByCourse(String courseName, String sessionType, String studentName) {
 		
-		if (course.equalsIgnoreCase("PSD3")) {
+		if (courseName.equalsIgnoreCase("PSD3")) {
 			
 			if (sessionType.equalsIgnoreCase("lab")) {
 				
-				if (username.equalsIgnoreCase("martynas"))
+				if (studentName.equalsIgnoreCase("martynas"))
 					return MARTYNAS_PSD3_LAB_ATTENDANCE;
-				else if (username.equalsIgnoreCase("helen"))
+				else if (studentName.equalsIgnoreCase("helen"))
 					return HELEN_PSD3_LAB_ATTENDANCE;
-				else if (username.equalsIgnoreCase("raluca"))
+				else if (studentName.equalsIgnoreCase("raluca"))
 					return RALUCA_PSD3_LAB_ATTENDANCE;
-				else if (username.equalsIgnoreCase("vlad"))
+				else if (studentName.equalsIgnoreCase("vlad"))
 					return VLAD_PSD3_LAB_ATTENDANCE;		
-				else if (username.equalsIgnoreCase("tomasz"))
+				else if (studentName.equalsIgnoreCase("tomasz"))
 					return TOMASZ_PSD3_LAB_ATTENDANCE;
 				
 			}			
@@ -36,5 +43,34 @@ public class StudentAttendance {
 		/* Oh snap! */
 		return null;
 		
-	} 	
+	}
+	
+	//That is the most ugly code I have ever written xD  This whole class has to be changed or we need a database.
+	//Write attendance from all the courses that selected student attended
+	public void generateCsvFile(String studentName,String courseName,String sessionType,String fileNameCSV){
+		String[] records = this.getAttendanceByCourse(courseName,sessionType,studentName);
+		try{
+			FileWriter writer = new FileWriter(fileNameCSV);
+			if (courseName.equalsIgnoreCase("PSD3") && sessionType.equalsIgnoreCase("lab")) {
+				writer.append("PSD3");
+				writer.append(',');
+				writer.append("lab");
+				writer.append(',');
+		  		for(String record: records){
+			    		writer.append(record);
+			    		writer.append(',');
+				}
+		    		writer.append('\n');
+	 
+		    		writer.flush();
+		    		writer.close();
+		    		
+			}
+			}
+			catch(IOException e){
+		     		e.printStackTrace();
+			} 
+		} 	
+
 }
+
