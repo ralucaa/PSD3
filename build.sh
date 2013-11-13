a1ca0094483616ef268588e2acda554ed98317d7
HelenFoster <0904275f@student.gla.ac.uk>
2013-11-13 20:21:35 +0000

    changed build script

--- build.sh
+++ build.sh
@@ -2,6 +2,4 @@
 #compile the Java files without Eclipse
 
 OPTS="-sourcepath src -d bin"
-javac $OPTS src/DatabaseInteraction/*.java
-javac $OPTS src/Menus/*.java
-javac $OPTS src/Objects/*.java
+javac $OPTS src/DatabaseInteraction/*.java src/Menus/*.java src/Objects/*.java
