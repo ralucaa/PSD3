13eec1506d1371ab0114ac471b1d25e67954d3cb
Martynas <martynas.buivys@gmail.com>
2013-11-30 19:19:27 +0000

    added some front end logic and added values to select dropdowns

--- Sprint2/SPRINT2_VIEWS/js/main.js
+++ Sprint2/SPRINT2_VIEWS/js/main.js
@@ -1,3 +1,18 @@
 $(function() {
+    // Set up datepicker
     $( ".date_picker" ).datepicker();
+    
+    // Session frequency listener
+    var sessionFrequency = $( "#session_frequency" );
+    sessionFrequency.change(function() {
+        theValue = sessionFrequency.val();
+        additionalSettings = $( "#repeat_until_wrapper" );
+        if (theValue == "0") {
+            additionalSettings.addClass( "hidden" );
+        } else {
+            additionalSettings.removeClass( "hidden" );
+        }
+        
+    });
+    
 });
