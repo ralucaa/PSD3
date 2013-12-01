9c41cf678931d835ed8aff3173d04baa9375275f
Vlad Iulian Schnakovszki <shnako@yahoo.com>
2013-12-01 18:44:48 +0000

    Very basic Hello World test. Moving on to ModelAndView objects.

--- Sprint2/Prototype2/src/main/java/uk/gla/ac/psd3/Prototype2/XController.java
+++ Sprint2/Prototype2/src/main/java/uk/gla/ac/psd3/Prototype2/XController.java
@@ -2,22 +2,30 @@ package uk.gla.ac.psd3.Prototype2;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 public class XController {
 	
-	@RequestMapping("/setupsession")
+	@RequestMapping(value = "/addsession", method = RequestMethod.POST)
 	@ResponseBody
-	String setupSession(){
+	String addSession(){
 		return "Setup session shown.";
 	}
 	
-	@RequestMapping("/viewsessions")
+	@RequestMapping(value = "/viewsessions", method = RequestMethod.GET)
 	@ResponseBody
-	String viewSessions(){
-		return "View sessions shown.";
+	String viewSessions(@RequestParam(value="student_id", required=true) String student_id, ModelMap model){
+		//Get the sessions from the database. If student_id is invalid show an error message.
+		
+		//Add the Session IDs as parameters to the ModelMap.
+		model.addAttribute("student_id", student_id);
+		
+		return "View sessions shown. Student ID: " + student_id;
 	}
 }
